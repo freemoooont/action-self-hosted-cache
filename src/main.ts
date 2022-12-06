fad756c738fb258a66e0c068c290aa77fafb1edf
Vladislav <freemooont@gmail.com>
2022-12-06 14:02:20 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ async function run(): Promise<void> {
     try{
         const depPath = tc.find('dep', '0.0.1');
         core.info(`depPath: ${depPath}`);
-        if(depPath){
-            await tc.extractTar(depPath, './');
-        } else {
-            await tc.extractTar('../../_tool/deps/0.0.1/node_modules.tar.gz', './');
-        }
+        // if(depPath){
+        //     await tc.extractTar(depPath, './');
+        // } else {
+        //     await tc.extractTar('../../_tool/deps/0.0.1/node_modules.tar.gz', './');
+        // }
     }catch ({message}) {
         core.setFailed(message as string);
     }
