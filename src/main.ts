46c6523f17c93bb8c251b041c42101e7a06e5aa3
Vladislav <freemooont@gmail.com>
2022-12-06 13:58:08 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import * as tc from "@actions/tool-cache";
 async function run(): Promise<void> {
     try{
         const depPath = tc.find('dep', '0.0.1');
-        await tc.extractTar(depPath, './');
+        core.info(`depPath: ${depPath}`);
+        if(depPath){
+            await tc.extractTar(depPath, './');
+        } else {
+            await tc.extractTar('../../_tool/deps/0.0.1/node_modules.tar.gz', './');
+        }
     }catch ({message}) {
         core.setFailed(message as string);
     }
